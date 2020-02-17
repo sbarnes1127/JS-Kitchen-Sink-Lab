6fc8b46b6745e7e175e74f22fde6d13f041f5f4a
Sierra Barnes <sierrabarnes@Sierras-MacBook-Pro.local>
2020-02-17 17:16:54 -0500

    finished excercize and it resembles the demo

--- app.js
+++ app.js
@@ -9,11 +9,58 @@ function sayHello() {
     alert('Hello World!');
 }
 
-console.log(sayHello());
+sayHello()
+
 
 function checkAge(name, age) {
-    if (age > 21) {
-        alert("Sorry" + name + ",you aren't old enough to view this page!");
+    if (age < 21) {
+        alert("Sorry " + name + ", you aren't old enough to view this page!");
     }
 }
 
+checkAge('Charles', 21);
+checkAge('Abby', 27);
+checkAge('James', 18);
+checkAge('John', 17);
+
+var favVeg = ['Spinach', 'Carrots', 'Greens', 'Brussel Sprouts'];
+
+for (var i = 0; i < favVeg.length; ++i) {
+    console.log(favVeg[i]);
+}
+
+var Peer = [{
+    name: 'John',
+    age: 23,
+},
+{
+    name: 'Rachel',
+    age: 22,
+},
+{
+    name: 'Forrest',
+    age: 20,
+},
+{
+    name: 'Ethan',
+    age: 17,
+},
+{
+    name: 'Lily',
+    age: 13,
+}]
+
+for (let i = 0; i < Peer.length; i++) {
+    let x = Peer[i].name
+    let y = Peer[i].age
+    checkAge(x, y);
+};
+
+function getLength(someWord) {
+    return someWord.length
+}
+let x= getLength('Hello World');
+
+
+if (x % 2 ===0) {console.log('The world is nice and even!')
+} else {console.log('The world is an odd place!')}
