3b3859f52750d82fc057ada48cbc304fd6a3ccf2
Sierra Barnes <sierrabarnes@Sierras-MacBook-Pro.local>
2020-02-17 14:39:28 -0500

    finished steps 1-7in lab and started checkAge function

---
+++ app.js
@@ -0,0 +1,19 @@
+var FullName = 'Sierra Barnes';
+
+const TotalStates = 50
+
+var a = 5 + 4;
+
+
+function sayHello() {
+    alert('Hello World!');
+}
+
+console.log(sayHello());
+
+function checkAge(name, age) {
+    if (age > 21) {
+        alert("Sorry" + name + ",you aren't old enough to view this page!");
+    }
+}
+
